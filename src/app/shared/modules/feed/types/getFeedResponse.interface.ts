b02341784397085f8096af57b2ffa1e8cfca814f
Eduard Toma <eduard.daniel.toma@gmail.com>
2021-05-10 19:28:20 +0300

    no message

--- src/app/shared/modules/feed/types/getFeedResponse.interface.ts
+++ src/app/shared/modules/feed/types/getFeedResponse.interface.ts
@@ -1,7 +1,7 @@
 import { ArticleInterface } from './../../../types/article.interface';
 export interface GetFeedResponseInterface {
 
-  article: ArticleInterface[];
-  articleCount: number;
+  articles: ArticleInterface[];
+  articlesCount: number;
 
 }
